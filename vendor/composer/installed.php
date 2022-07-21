0d3a95f7d434b325ac80f97092647cfe4c692bb8
Dmitriy Mukin <dev@xtrade.net.ru>
2022-07-21 11:55:46 +0600

    Add initial set of files

---
+++ vendor/composer/installed.php
@@ -0,0 +1,54 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'v5.4.99',
+    'version' => '5.4.99.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'symfony/website-skeleton',
+  ),
+  'versions' => 
+  array (
+    'symfony/flex' => 
+    array (
+      'pretty_version' => 'v1.19.2',
+      'version' => '1.19.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'd1a692369be53445af6e391170b509d7f5d026cf',
+    ),
+    'symfony/polyfill-ctype' => 
+    array (
+      'replaced' => 
+      array (
+        0 => '*',
+      ),
+    ),
+    'symfony/polyfill-iconv' => 
+    array (
+      'replaced' => 
+      array (
+        0 => '*',
+      ),
+    ),
+    'symfony/polyfill-php72' => 
+    array (
+      'replaced' => 
+      array (
+        0 => '*',
+      ),
+    ),
+    'symfony/website-skeleton' => 
+    array (
+      'pretty_version' => 'v5.4.99',
+      'version' => '5.4.99.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+  ),
+);
